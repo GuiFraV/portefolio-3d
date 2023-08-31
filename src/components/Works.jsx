b171835cf7c26b0872c422e737c8f657e8d006a5
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-08-31 17:19:56 +0200

    add ball skills

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+    </>
   )
 }
 
